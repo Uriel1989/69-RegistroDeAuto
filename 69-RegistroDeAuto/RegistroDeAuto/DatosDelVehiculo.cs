f678d7717ac9d364c72daa8371c990f947008de6
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 21:28:37 -0300

    Add files via upload

---
+++ 69-RegistroDeAuto/RegistroDeAuto/DatosDelVehiculo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RegistroDeAuto
+{
+    class DatosDelVehiculo
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string TipoCombustible { get; set; }
+        public int CantPuertas { get; set; }
+
+        public string presentacion()
+        {
+            return "Marca: " + " " + this.Marca + ", modelo" + " " + this.Modelo + ", combustible"+ " " + this.TipoCombustible + ", numero de puertas " + " " + this.CantPuertas;
+        }
+    }
+}
---
+++ 69-RegistroDeAuto/RegistroDeAuto/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RegistroDeAuto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<DatosDelVehiculo> ListAutos = new List<DatosDelVehiculo>();
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.CursorLeft = 30;
+
+            Console.WriteLine("Bienvenido al registro de autos");
+
+            Console.WriteLine("Ingrese 'S' para el ingreso de datos o 'N' para cerrar el programa:");
+
+            string acceso = Console.ReadLine();
+
+            while (acceso == "s")
+            {
+                DatosDelVehiculo coche = new DatosDelVehiculo();
+
+                Console.WriteLine("ingresa la marca :");
+
+                coche.Marca = Console.ReadLine();
+
+                Console.WriteLine("Ingrese el modelo:");
+
+                coche.Modelo = Console.ReadLine();
+
+                Console.WriteLine("Tipo de combustible:");
+
+                coche.TipoCombustible = Console.ReadLine();
+
+                Console.WriteLine("Cantidad de puertas:");
+
+                coche.CantPuertas = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("¿Desea ingresar mas vehiculos? s/n:");
+
+                acceso = Console.ReadLine();
+
+                ListAutos.Add(coche);
+            }
+
+            for ( int i = 0; i< ListAutos.Count; i++)
+            {
+                DatosDelVehiculo datos = ListAutos[i];
+
+                Console.WriteLine(datos.presentacion());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
